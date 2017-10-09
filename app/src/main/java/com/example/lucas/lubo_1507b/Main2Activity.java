8c58e3a3bf0777e1943a8087179876a80113b00c
lucas <luzezxc@gmail.com>
2017-10-09 15:56:42 +0800

    第一个分支完成test->intent

--- app/src/main/java/com/example/lucas/lubo_1507b/Main2Activity.java
+++ app/src/main/java/com/example/lucas/lubo_1507b/Main2Activity.java
@@ -5,15 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
-
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnBack;
+    public static final String TCC = "hoho";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = this.getIntent();
+        Toast.makeText(this,intent.getStringExtra(MainActivity.TGC),Toast.LENGTH_SHORT).show();
 
+        initBtnShow();
+    }
+
+    private void initBtnShow() {
+       btnBack = (Button) findViewById(R.id.btn_second);
+        btnBack.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btn_second:
+                intent = new Intent(this,MainActivity.class);
+                intent.putExtra(TCC,"I'm back");
+                startActivity(intent);
+                this.finish();
+                break;
+
+        }
+    }
 }
--- app/src/main/java/com/example/lucas/lubo_1507b/MainActivity.java
+++ app/src/main/java/com/example/lucas/lubo_1507b/MainActivity.java
@@ -5,15 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnJump;
-    private static final String TGC = "hello";
+    public static final String TGC = "hello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initBtnShow();
+        Intent intent = this.getIntent();
+        Toast.makeText(this,intent.getStringExtra(Main2Activity.TCC),Toast.LENGTH_SHORT).show();
     }
     private void initBtnShow() {
         btnJump = (Button) findViewById(R.id.btn_jump);
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.btn_jump:
                 Intent intent = new Intent(this, Main2Activity.class);
-                intent.putExtra("TGC","I'm coming");
+                intent.putExtra(TGC,"I'm coming");
                 startActivity(intent);
                 this.finish();
                 break;
