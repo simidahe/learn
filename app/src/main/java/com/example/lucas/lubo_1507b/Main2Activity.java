b37daa48fcc6f0176a2f352273ef65f873c89a9c
lucas <luzezxc@gmail.com>
2017-10-09 15:25:13 +0800

    第一个Activity跳转第二个Activity

--- app/src/main/java/com/example/lucas/lubo_1507b/Main2Activity.java
+++ app/src/main/java/com/example/lucas/lubo_1507b/Main2Activity.java
@@ -6,24 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
-    private Button btnJump;
+public class Main2Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        initBtnShow();
-    }
-
-    private void initBtnShow() {
-        btnJump = (Button) findViewById(R.id.btn_jump);
-        btnJump.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(this, Main2Activity.class);
-
 
     }
+
+
 }
--- app/src/main/java/com/example/lucas/lubo_1507b/MainActivity.java
+++ app/src/main/java/com/example/lucas/lubo_1507b/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.lucas.lubo_1507b;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnJump;
+    private static final String TGC = "hello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBtnShow();
+    }
+    private void initBtnShow() {
+        btnJump = (Button) findViewById(R.id.btn_jump);
+        btnJump.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_jump:
+                Intent intent = new Intent(this, Main2Activity.class);
+                intent.putExtra("TGC","I'm coming");
+                startActivity(intent);
+                this.finish();
+                break;
+
+        }
+
     }
 }
